c91b19dde9f5882d80ba3c3aba179f3a9e4b302a
¨Fellipe <fellipescirea@gmail.com>
2025-11-16 19:57:58 -0800

    prep work for layout modification

---
+++ Assignment 07/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * @ Author: Fellipe M Fumagali Scirea
+ * @ BCIT Student # : A01181350
+ * @ Created: 11-16-2025
+ * @ Description: COMP 2132 Assignment 07 - Deliverable as part of coursework.
+ */
+
+// jQuery Document Ready - Ensures the DOM is fully loaded before executing scripts
+$(document).ready(function () {
+
+
+});
